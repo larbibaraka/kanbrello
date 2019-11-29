4e2fba723936d9f8bd046432fc007a9d2d92c201
Baraka Larbi <laridev@gmail.com>
2019-11-29 17:40:48 +0100

    adding two main routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import Login from './Components/Login'; 
-import Dashboard from './Components/Dashboard';
-import {  BrowserRouter as Router,  Switch,  Route,  Link} from "react-router-dom";
-import './App.css';
+import React from "react";
+import Login from "./Components/Login";
+import Dashboard from "./Components/Dashboard";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
 
 const App: React.FC = () => {
   return (
-    <Switch>
-      <Route path="/">
-        <Login/>
-      </Route>
-      <Route path="/dashboard">
-        <Dashboard/>
-      </Route>
-    </Switch>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
