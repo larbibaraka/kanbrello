0a37bb109ef6127178135cd084593f75d4f26abc
Baraka Larbi <laridev@gmail.com>
2019-11-30 01:41:50 +0100

    removing things that does not matter

--- client/src/Components/DashboardElements/DashboardNavBar.tsx
+++ client/src/Components/DashboardElements/DashboardNavBar.tsx
@@ -25,6 +25,8 @@ const DashboardNavBar: React.FC = () => {
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
           <NavDropdown title="Boards" id="collasible-nav-dropdown">
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <NavDropdown.Divider />
             <Accordion defaultActiveKey="0">
               <Card>
                 <Card.Header>
@@ -46,18 +48,7 @@ const DashboardNavBar: React.FC = () => {
                   <Card.Body>Hello! I'm another body</Card.Body>
                 </Accordion.Collapse>
               </Card>
-            </Accordion>
-
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
+            </Accordion>                    
           </NavDropdown>
         </Nav>
         <Nav>
