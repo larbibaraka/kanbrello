97ff458d0fcc7a761b4c477996ae28ed340902c9
Baraka Larbi <laridev@gmail.com>
2019-11-30 14:42:56 +0100

    creating acordion element

--- client/src/Components/DashboardElements/DashboardNavBar.tsx
+++ client/src/Components/DashboardElements/DashboardNavBar.tsx
@@ -10,7 +10,8 @@ import {
   Card
 } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHome, faPlus, faStar, faStopwatch, faBookmark, faInfo, faInfoCircle, faBell, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faHome, faPlus, faStar, faStopwatch, faBookmark, faInfo, faInfoCircle, faBell, faUser, faAddressCard } from "@fortawesome/free-solid-svg-icons";
+import NavbarAcordion from "./navbarElements/NavbarAcordion";
 
 const DashboardNavBar: React.FC = () => {
   return (
@@ -24,52 +25,16 @@ const DashboardNavBar: React.FC = () => {
           <NavDropdown title="Boards" id="collasible-nav-dropdown">
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
             <NavDropdown.Divider />
-            <Accordion defaultActiveKey="0" style={{width : '25rem'}}>
-              <Card>
-                <Card.Header>
-                <h6 style={{lineHeight :2.2}}><FontAwesomeIcon icon={faStar} /> STARRED BOARDS <Accordion.Toggle as={Button} variant="light" eventKey="0" style={{float: 'right'}}>
-                   <FontAwesomeIcon icon={faPlus} />
-                  </Accordion.Toggle></h6>
-                  
-                </Card.Header>
-                <Accordion.Collapse eventKey="0">
-                  <Card.Body>Hello! I'm the body</Card.Body>
-                </Accordion.Collapse>
-              </Card>
-            </Accordion>                    
-            <Accordion   style={{width : '25rem'}}>
-              <Card>
-                <Card.Header>
-                <h6 style={{lineHeight :2.2}}><FontAwesomeIcon icon={faStopwatch} /> RECENT BOARD <Accordion.Toggle as={Button} variant="light" eventKey="0" style={{float: 'right'}}>
-                   <FontAwesomeIcon icon={faPlus} />
-                  </Accordion.Toggle></h6>
-                </Card.Header>
-                <Accordion.Collapse eventKey="0">
-                  <Card.Body>Hello! I'm the body</Card.Body>
-                </Accordion.Collapse>
-              </Card>
-            </Accordion> 
-            <Accordion  style={{width : '25rem'}}>
-              <Card>
-                <Card.Header>
-                <h6 style={{lineHeight :2.2}}><FontAwesomeIcon icon={faBookmark} /> PERSONAL BOARD <Accordion.Toggle as={Button} variant="light" eventKey="0" style={{float: 'right'}}>
-                   <FontAwesomeIcon icon={faPlus} />
-                  </Accordion.Toggle></h6>
-                </Card.Header>
-                <Accordion.Collapse eventKey="0">
-                  <Card.Body>Hello! I'm the body</Card.Body>
-                </Accordion.Collapse>
-              </Card>
-            </Accordion> 
+            <NavbarAcordion title="STARRED BOARDS" icon={faStar} body="...." />
+            <NavbarAcordion title="RECENT BOARD" icon={faStopwatch} body="...." />
+            <NavbarAcordion title="PERSONAL BOARD" icon={faBookmark} body="...." /> 
             <NavDropdown.Divider />
            <div className="links-boards">
                 <Button variant="info" block>Create new Board...</Button>
                 <Button variant="info" block>See Closed Boards...</Button>
            </div>
-      
           </NavDropdown>
           <FormControl type="text" placeholder="Search" className="ml-10" />
-          
         </Nav>
         <Nav>
           <Nav.Link href="#"><Button variant="light"><FontAwesomeIcon icon={faPlus}/></Button></Nav.Link>
