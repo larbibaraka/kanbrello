df2c37c68087aec9daa87f6f45284be85af567ac
Baraka Larbi <laridev@gmail.com>
2019-12-08 21:19:44 +0100

    last commit for navbar for now

--- client/src/Components/DashboardElements/DashboardNavBar.tsx
+++ client/src/Components/DashboardElements/DashboardNavBar.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import {
   Navbar,
-  Form,
   FormControl,
   Button,
   Nav,
   NavDropdown,
-  Accordion,
-  Card
+  Dropdown,
+  DropdownButton
 } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome, faPlus, faStar, faStopwatch, faBookmark, faInfo, faInfoCircle, faBell, faUser, faAddressCard } from "@fortawesome/free-solid-svg-icons";
@@ -36,8 +35,17 @@ const DashboardNavBar: React.FC = () => {
           </NavDropdown>
           <FormControl type="text" placeholder="Search" className="ml-10" />
         </Nav>
-        <Nav>
-          <Nav.Link href="#"><Button variant="light"><FontAwesomeIcon icon={faPlus}/></Button></Nav.Link>
+        <Nav> 
+          <Nav.Link href="#">
+          <DropdownButton id="dropdown-item-button" title="Boards">
+            <div>
+              <h6 className="text-center">Create</h6>
+              <hr/>
+            </div>
+            <Dropdown.Item as="button">Create Board</Dropdown.Item>
+            <Dropdown.Item as="button">Create Team</Dropdown.Item>
+          </DropdownButton>
+          </Nav.Link>
           <Nav.Link href="#"><Button variant="light"><FontAwesomeIcon icon={faInfoCircle}/></Button></Nav.Link>
           <Nav.Link href="#"><Button variant="light"><FontAwesomeIcon icon={faBell}/></Button></Nav.Link>
           <Nav.Link href="#"><Button variant="light"><FontAwesomeIcon icon={faUser}/></Button></Nav.Link>
